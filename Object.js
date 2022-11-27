f8164ed42edbc574234a89e8b04008062ab3f7b9
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2022-11-27 21:21:36 +0700

    สร้าง obj
    ทดสอบการทำงาน ของ obj

---
+++ Object.js
@@ -0,0 +1,17 @@
+const name = "wow"
+const number = 55
+
+const customer ={
+    customer : "Ae",
+    age:22,
+    address: "Bansansai" //เก็บข้อมูลของตัวเเปร
+}
+console.log(customer);
+
+//การกำหนดตัวเเปรก่อนที่จะประกาศ
+const username ={
+    username :name,
+    number :number,
+ //เก็บข้อมูลของตัวเเปร
+}
+console.log(username);
